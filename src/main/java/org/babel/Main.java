78595f425f18a01ec5aeabdfe10c74d0f2cb0ef5
Fernando Sanz Almau <fernando.sanz@babelgroup.com>
2024-02-22 12:38:19 +0100

    Modificación de main

--- src/main/java/org/babel/Main.java
+++ src/main/java/org/babel/Main.java
@@ -1,14 +1,26 @@
 package org.babel;
 
+import com.google.gson.Gson;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
+
         // Crear un objeto Java
         Persona persona = new Persona("Juan", 25);
 
+        // Convertir objeto Java a JSON
+        Gson gson = new Gson();
+        String json = gson.toJson(persona);
+        System.out.println("Objeto Java convertido a JSON:");
+        System.out.println(json);
+
+        // Convertir JSON a objeto Java
+        String jsonPersona = "{\"nombre\":\"María\",\"edad\":30}";
+        Persona personaDesdeJson = gson.fromJson(jsonPersona, Persona.class);
+        System.out.println("\nJSON convertido a objeto Java:");
+        System.out.println("Nombre: " + personaDesdeJson.getNombre());
+        System.out.println("Edad: " + personaDesdeJson.getEdad());
     }
 }
