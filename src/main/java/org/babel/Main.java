44eb22e94c445b27b7e31ce043d5d1a41399e275
Fernando Sanz Almau <fernando.sanz@babelgroup.com>
2024-02-22 12:26:35 +0100

    Creación Persona

--- src/main/java/org/babel/Main.java
+++ src/main/java/org/babel/Main.java
@@ -7,6 +7,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        // Crear un objeto Java
+        Persona persona = new Persona("Juan", 25);
 
     }
 }
